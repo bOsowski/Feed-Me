3a02f725262da3db3e74f1a75d382fc3c6865f98
Bartosz Osowski <bartosz.osowski@errigal.com>
2018-11-29 19:11:56 +0000

    Set up the background, crocodile and the structure of the game.

--- Feed Me/GameScene.swift
+++ Feed Me/GameScene.swift
@@ -1,89 +1,81 @@
-//
-//  GameScene.swift
-//  Feed Me
-//
-//  Created by Bartosz Osowski on 09/11/2018.
-//  Copyright © 2018 Bartosz Osowski. All rights reserved.
-//
-
 import SpriteKit
-import GameplayKit
+
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var crocodile: SKSpriteNode!
+    private var prize: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        //setUpPhysics()
+        setUpScenery()
+        //setUpPrize()
+        //setUpVines()
+        setUpCrocodile()
+        //setUpAudio()
+    }
+    
+    //MARK: - Level setup
+    
+    fileprivate func setUpPhysics() { }
+    fileprivate func setUpScenery() {
+        let background:SKSpriteNode = SKSpriteNode(imageNamed: ImageName.Background)
+        background.position = CGPoint(x: 0,y: 0)
+        background.anchorPoint = CGPoint(x: 0,y: 0)
+        background.zPosition = Layer.Background
+        background.size = self.size
+        addChild(background)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let water:SKSpriteNode = SKSpriteNode(imageNamed: ImageName.Water)
+        water.position = background.position
+        water.anchorPoint = background.anchorPoint
+        water.zPosition = Layer.Water
+        water.size.width = self.size.width
+        water.size.height = self.size.height / 100 * 21.39
+        addChild(water)
+    }
+    
+    fileprivate func setUpPrize() { }
+    
+    //MARK: - Vine methods
+    
+    fileprivate func setUpVines() { }
+    
+    //MARK: - Croc methods
+    
+    fileprivate func setUpCrocodile() {
+        crocodile = SKSpriteNode(imageNamed: ImageName.CrocMouthClosed)
+        crocodile.physicsBody = SKPhysicsBody()
+        crocodile.position = CGPoint(x: self.size.width * 0.75, y: self.size.height * 0.312)
+        crocodile.zPosition = Layer.Crocodile
+        crocodile.physicsBody?.usesPreciseCollisionDetection = true
+        crocodile.physicsBody?.categoryBitMask = 0
+        crocodile.physicsBody?.categoryBitMask = PhysicsCategory.Crocodile
+        crocodile.physicsBody?.contactTestBitMask = PhysicsCategory.Prize
+        crocodile.physicsBody?.isDynamic = false
+        addChild(crocodile)
+        animateCrocodile()
+    }
+    
+    fileprivate func animateCrocodile() {
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
+    fileprivate func runNomNomAnimationWithDelay(_ delay: TimeInterval) { }
     
+    //MARK: - Touch handling
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) { }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) { }
+    fileprivate func showMoveParticles(touchPosition: CGPoint) { }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
+    //MARK: - Game logic
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+    override func update(_ currentTime: TimeInterval) { }
+    func didBegin(_ contact: SKPhysicsContact) { }
+    fileprivate func checkIfVineCutWithBody(_ body: SKPhysicsBody) { }
+    fileprivate func switchToNewGameWithTransition(_ transition: SKTransition) { }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
+    //MARK: - Audio
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+    fileprivate func setUpAudio() { }
 }
